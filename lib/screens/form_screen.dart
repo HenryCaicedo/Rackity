1bfa731edf1f9f979474486ed54ee77182617da3
HenryCaicedo <72535346+HenryCaicedo@users.noreply.github.com>
2023-05-12 00:41:58 -0500

    Cambios reales formulario cámara

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -8,7 +8,6 @@ class FormScreen extends StatefulWidget {
 
 class _FormScreenState extends State<FormScreen> {
   String _selectedGarmentType = 'Top';
-  Color _selectedColor = Colors.black;
 
   List<String> _garmentTypes = [
     'Top',
@@ -16,120 +15,98 @@ class _FormScreenState extends State<FormScreen> {
     'Footwear',
   ];
 
-  List<Color> _colors = [
-    Colors.black,
-    Colors.white,
-    Colors.red,
-    Colors.green,
-    Colors.yellow,
-    Colors.blue,
-    Colors.brown,
+  List<String> _garmentTags = [
+    "Dresses",
+    "Outerwear",
+    "Activewear",
+    "Casual",
+    "Formal",
+    "Accessories",
+    "Swimwear"
   ];
+
+  List<String> _selectedTags = [];
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Material(
-        color: Color(0xFFBCDED6),
-        child: Container(
-          height: MediaQuery.of(context).size.height -
-              MediaQuery.of(context).padding.top,
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Color.fromARGB(255, 255, 255, 255),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color.fromRGBO(0, 0, 0, 0.847).withOpacity(0.08),
-                      spreadRadius: 1,
-                      blurRadius: 3,
-                      offset: Offset(0, 2),
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        color: Colors.transparent,
+        height: MediaQuery.of(context).size.height -
+            MediaQuery.of(context).padding.top,
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(35),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromRGBO(0, 0, 0, 0.847).withOpacity(0.08),
+                    spreadRadius: 1,
+                    blurRadius: 3,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: _garmentTypes
+                          .map(
+                            (type) => Row(
+                              children: [
+                                Radio(
+                                  value: type,
+                                  groupValue: _selectedGarmentType,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      _selectedGarmentType = value!;
+                                    });
+                                  },
+                                ),
+                                Text(type),
+                              ],
+                            ),
+                          )
+                          .toList(),
+                    ),
+                    Wrap(
+                      spacing: 2,
+                      runSpacing: 0,
+                      children: _garmentTags
+                          .map(
+                            (tag) => ChoiceChip(
+                              label: Text(tag),
+                              selected: _selectedTags.contains(tag),
+                              onSelected: (selected) {
+                                setState(() {
+                                  if (selected) {
+                                    _selectedTags.add(tag);
+                                  } else {
+                                    _selectedTags.remove(tag);
+                                  }
+                                });
+                              },
+                              selectedColor: Color.fromARGB(172, 228, 171,
+                                  101), // Set a brighter color here
+                              backgroundColor: Colors
+                                  .grey[300], // Adjust the background color
+                            ),
+                          )
+                          .toList(),
                     ),
                   ],
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Select Garment Type',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 16),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: _garmentTypes
-                            .map(
-                              (type) => Row(
-                                children: [
-                                  Radio(
-                                    value: type,
-                                    groupValue: _selectedGarmentType,
-                                    onChanged: (value) {
-                                      setState(() {
-                                        _selectedGarmentType = value!;
-                                      });
-                                    },
-                                  ),
-                                  Text(type),
-                                ],
-                              ),
-                            )
-                            .toList(),
-                      ),
-                      SizedBox(height: 16),
-                      Text(
-                        'Select Color',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 16),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: _colors
-                            .map(
-                              (color) => GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    _selectedColor = color;
-                                  });
-                                },
-                                child: CircleAvatar(
-                                  backgroundColor: color,
-                                  radius: 16,
-                                  child: color == _selectedColor
-                                      ? Icon(
-                                          Icons.check,
-                                          color: Colors.white,
-                                        )
-                                      : null,
-                                ),
-                              ),
-                            )
-                            .toList(),
-                      ),
-                      SizedBox(height: 32),
-                      ElevatedButton(
-                        onPressed: () {
-// Add the garment with the selected type and color
-                        },
-                        child: Text('Add'),
-                      ),
-                    ],
-                  ),
-                ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/tabs/camera_tab.dart
+++ lib/tabs/camera_tab.dart
@@ -1,16 +1,12 @@
 import 'dart:io';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import '../screens/form_screen.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:math';
-import 'package:firebase_storage/firebase_storage.dart';
-
 import 'package:shared_preferences/shared_preferences.dart';
-
 import '../data/database.dart';
+import '../colors.dart';
 
 class CameraTab extends StatefulWidget {
   @override
@@ -24,23 +20,6 @@ class _CameraTabState extends State<CameraTab> {
   Future<int?> obtenerIdUsuario() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.getInt('idUsuario');
-  }
-
-  Future<String> uploadPhotoToStorage(File file) async {
-    try {
-      final name =
-          'image_${DateTime.now().millisecondsSinceEpoch}_${Random().nextInt(1000)}.jpg';
-      final Reference storageReference =
-          FirebaseStorage.instance.ref().child('photos').child(name);
-      final TaskSnapshot snapshot = await storageReference.putFile(file);
-
-      // Obtiene la URL de descarga de la foto
-      final String downloadUrl = await snapshot.ref.getDownloadURL();
-      return downloadUrl;
-    } catch (e) {
-      print('Error uploading photo: $e');
-      return "error";
-    }
   }
 
   void saveImageToStorage(File imageFile, BuildContext context) async {
@@ -74,19 +53,6 @@ class _CameraTabState extends State<CameraTab> {
     });
   }
 
-  Future<void> addPhotoToCollection(String photoUrl) async {
-    try {
-      final CollectionReference photosCollection =
-          FirebaseFirestore.instance.collection('photos');
-      await photosCollection.add({
-        'url': photoUrl,
-        // Otros campos de datos relacionados con la foto
-      });
-    } catch (e) {
-      print('Error adding photo to collection: $e');
-    }
-  }
-
   @override
   void initState() {
     init = false;
@@ -95,57 +61,293 @@ class _CameraTabState extends State<CameraTab> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
+    //Widget luego de tomar la foto
+    Widget after = Expanded(
+      child: Container(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Center(
-                child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        _takePicture();
-                      });
-                    },
-                    child: Text('Tomar Foto'),
-                  ),
+            Expanded(
+                flex: 20,
+                child: Container(
+                  child: FormScreen(),
+                )),
+            Expanded(
+                flex: 24,
+                child: Container(
+                  child: init
+                      ? ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image(
+                            image: Image.file(_image).image,
+                          ),
+                        )
+                      : Container(),
+                )),
+            Expanded(
+                flex: 8,
+                child: Container(
+                  child: Center(
+                      child: Padding(
+                    padding: EdgeInsets.all(30.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                _takePicture();
+                              });
+                            },
+                            child: Text('Tomar Foto'),
+                          ),
+                        ),
+                        SizedBox(width: 16), // Espacio entre botones
+                        Expanded(
+                          flex: 1,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (init) {
+                                saveImageToStorage(_image, context);
+                                setState(() {
+                                  init = false;
+                                });
+                              }
+                            },
+                            child: Text('Guardar Foto'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )),
+                )),
+          ],
+        ),
+      ),
+    );
+
+    //Widget antes de tomar la foto
+    Widget before = Expanded(
+      child: Container(
+        child: Column(
+          children: [
+            Expanded(
+                flex: 20,
+                child: Container(
+                  child: FormScreen(),
+                )),
+            Expanded(
+                flex: 24,
+                child: Container(
+                  child: init
+                      ? ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image(
+                            image: Image.file(_image).image,
+                          ),
+                        )
+                      : Container(),
+                )),
+            Expanded(
+                flex: 8,
+                child: Container(
+                  child: Center(
+                      child: Padding(
+                    padding: EdgeInsets.all(30.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                _takePicture();
+                              });
+                            },
+                            child: Text('Tomar Foto'),
+                          ),
+                        ),
+                        SizedBox(width: 16), // Espacio entre botones
+                        Expanded(
+                          flex: 1,
+                          child: ElevatedButton(
+                            onPressed: null,
+                            child: Text('Guardar Foto'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )),
+                )),
+          ],
+        ),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              decoration:
+                  BoxDecoration(color: Theme.of(context).colorScheme.primary),
+              padding: EdgeInsets.only(
+                  left: 16.0, top: 32.0, right: 24.0, bottom: 16),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Agregar prenda',
+                style: TextStyle(
+                  fontSize: 36.0,
+                  fontWeight: FontWeight.w500,
+                  color: textColor,
                 ),
-                SizedBox(width: 16), // Espacio entre botones
-                Expanded(
-                  flex: 1,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (init) {
-                        saveImageToStorage(_image, context);
-                        upPhoto();
-                        setState(() {
-                          init = false;
-                        });
-                      }
-                    },
-                    child: Text('Guardar Foto'),
-                  ),
-                ),
-              ],
-            )),
-            init ? Image.file(_image) : Container(),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary),
+                  child: Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF2F2F2),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40.0),
+                          topRight: Radius.circular(40.0),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.transparent,
+                            ),
+                            padding: EdgeInsets.only(
+                                left: 26.0, top: 0, right: 24.0, bottom: 0),
+                            alignment: Alignment.centerLeft,
+                          ),
+                          init ? after : before,
+                        ],
+                      ))),
+            )
           ],
         ),
       ),
     );
   }
+}
 
-  void upPhoto() async {
-    String photoUrl = await uploadPhotoToStorage(_image);
-    if (photoUrl != null) {
-      await addPhotoToCollection(photoUrl);
-    } else {
-      // Ocurrió un error al subir la foto al almacenamiento
-    }
+//Formulario
+class FormScreen extends StatefulWidget {
+  @override
+  _FormScreenState createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  String _selectedGarmentType = 'Top';
+
+  List<String> _garmentTypes = [
+    'Top',
+    'Bottom',
+    'Footwear',
+  ];
+
+  List<String> _garmentTags = [
+    "Dresses",
+    "Outerwear",
+    "Activewear",
+    "Casual",
+    "Formal",
+    "Accessories",
+    "Swimwear"
+  ];
+
+  List<String> _selectedTags = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        color: Colors.transparent,
+        height: MediaQuery.of(context).size.height -
+            MediaQuery.of(context).padding.top,
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(35),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromRGBO(0, 0, 0, 0.847).withOpacity(0.08),
+                    spreadRadius: 1,
+                    blurRadius: 3,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: _garmentTypes
+                          .map(
+                            (type) => Row(
+                              children: [
+                                Radio(
+                                  value: type,
+                                  groupValue: _selectedGarmentType,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      _selectedGarmentType = value!;
+                                    });
+                                  },
+                                ),
+                                Text(type),
+                              ],
+                            ),
+                          )
+                          .toList(),
+                    ),
+                    Wrap(
+                      spacing: 2,
+                      runSpacing: 0,
+                      children: _garmentTags
+                          .map(
+                            (tag) => ChoiceChip(
+                              label: Text(tag),
+                              selected: _selectedTags.contains(tag),
+                              onSelected: (selected) {
+                                setState(() {
+                                  if (selected) {
+                                    _selectedTags.add(tag);
+                                  } else {
+                                    _selectedTags.remove(tag);
+                                  }
+                                });
+                              },
+                              selectedColor: Color.fromARGB(172, 228, 171,
+                                  101), // Set a brighter color here
+                              backgroundColor: Colors
+                                  .grey[300], // Adjust the background color
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
